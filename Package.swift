ba6007742f86aff654d73fbeed60fcc3ae103469
Ming Chen <ming@localhost>
2024-07-22 20:38:35 -0700

    Update target to iOS 13

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "CocoaAsyncSocket",
     platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v9)
+        .iOS(.v13),
+        .macOS(.v10_13),
+        .tvOS(.v13)
     ],
     products: [
         .library(
